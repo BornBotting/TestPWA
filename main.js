fc1f09130d3eb505ee4b892fd00c719958bdaf90
BornBotting <andreat1998@hotmail.com>
2025-08-05 11:49:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
 const audioFiles = ['audio1.mp3','audio2.mp3','audio3.mp3','audio4.mp3'];
 const audios = {};
-audioFiles.forEach(file => audios[file] = new Audio(file));
+let currentButtonId = null;
 
-function playAudio(file) {
-  const a = audios[file];
-  a.currentTime = 0;
-  a.play();
+audioFiles.forEach(file => {
+  const audio = new Audio(file);
+  audio.addEventListener('ended', () => {
+    if (currentButtonId) {
+      document.getElementById(currentButtonId).classList.remove('playing');
+      currentButtonId = null;
+    }
+  });
+  audios[file] = audio;
+});
+
+function playAudio(file, buttonId) {
+  // Se un altro audio è in riproduzione, lo resettiamo
+  if (currentButtonId && currentButtonId !== buttonId) {
+    const prevFile = Object.keys(audios).find(key => !audios[key].paused);
+    if (prevFile) {
+      audios[prevFile].pause();
+      audios[prevFile].currentTime = 0;
+      document.getElementById(currentButtonId).classList.remove('playing');
+    }
+  }
+
+  const audio = audios[file];
+  const btn = document.getElementById(buttonId);
+
+  if (audio.paused) {
+    audio.currentTime = 0;
+    audio.play();
+    btn.classList.add('playing');
+    currentButtonId = buttonId;
+  } else {
+    audio.pause();
+    btn.classList.remove('playing');
+    currentButtonId = null;
+  }
 }
